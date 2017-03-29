c6e8aebb61692daa3728689ad174b3ec63845fd9
hmozju <huangmin_ustc@163.com>
2017-03-29 21:44:53 +0800

    imagesc instance

---
+++ imagesc example/imagesc example.m
@@ -0,0 +1,35 @@
+
+clear all;
+
+clc;
+
+x = [0:0.01:10-0.01];
+
+y = [0:0.01:10-0.01];
+
+m = length(x);
+
+n = length(y);
+
+for i = [1:m];
+    
+    for j = [1:n];
+        
+        f(i,j) = x(i) + y(j);
+        
+    end;
+    
+end;
+
+for i = [1:m]
+    
+    for j = [1:n]
+        
+f_max = max(f(i,j));
+
+    end;
+end;
+
+imagesc(x,y',f/f_max);
+
+colorbar;
